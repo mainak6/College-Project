ea5a615933cb8f86e8778b68d57cd17faa8fd0eb
MAINAK <senriju212@gmail.com>
2023-08-27 00:00:07 +0530

    first commit

---
+++ light on and off/app.js
@@ -0,0 +1,8 @@
+function onthe(){
+    let btn=document.querySelector(".offlight");
+    btn.setAttribute("src", "lightOn.png");
+}
+function offthe(){
+    let btnn=document.querySelector(".offlight");
+    btnn.setAttribute("src", "lightOff.png");
+}
